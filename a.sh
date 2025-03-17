2d18b20390559e926a49e679561ba0c9901537f7
Krisna DM <43299860+MortyID@users.noreply.github.com>
2025-03-17 13:36:55 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -20,6 +20,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+source ~/.bashrc
+
 # Cek apakah NVM berhasil diinstal
 if command -v nvm &> /dev/null
 then
